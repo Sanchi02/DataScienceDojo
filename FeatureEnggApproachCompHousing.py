4a9f7967aac1bc83a5a6117afb8189d03dd1463e
sanchi002 <sanchibadkas@gmail.com>
2019-08-09 12:56:11 +0530

    Some progress with feature engg

--- FeatureEnggApproachCompHousing.py
+++ FeatureEnggApproachCompHousing.py
@@ -20,16 +20,19 @@ def idOutliers(dat):
 train_file_path = 'C:\\Users\\Sanchi\\Desktop\\Stuff\\DataScienceSiraj\\data\\housing_train.csv'
 test_data_path = 'C:\\Users\\Sanchi\\Desktop\\Stuff\\DataScienceSiraj\\data\\housing_test.csv'
 
-train_data = pd.read_csv(train_file_path)
+train_data_raw = pd.read_csv(train_file_path)
 test_data = pd.read_csv(test_data_path)
-
-y = train_data.SalePrice
+# print(train_data_raw.shape)
+# print(test_data.shape)
+y = train_data_raw.SalePrice
 features = ['LotArea', 'LotFrontage', 'YearBuilt', 'YearRemodAdd', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-X = train_data[features]
+X = train_data_raw[features]
 
 #Trying to prevent "Data leakage"
-train_data = train_data.drop('SalePrice', axis=1)
+train_data = train_data_raw.drop('SalePrice', axis=1)
 all_data = pd.concat([train_data, test_data], ignore_index=True).copy()
+print(train_data.shape)
+print(test_data.shape)
 
 test  = all_data[all_data.LotFrontage.isnull()]
 train = all_data[~all_data.LotFrontage.isnull()]
@@ -80,4 +83,53 @@ for col in (set(X_train_lotFrontage.columns) - set(X_test.columns)):
 X_test = X_test[X_train_lotFrontage.columns]
 all_data.loc[all_data.LotFrontage.isnull(), 'LotFrontage'] = clf.predict(X_test)
 
+#Separating Test data and sample data post feature engineering
+X_train_post = all_data[:train_data.shape[0]]
+X_test_post = all_data[:test_data.shape[0]]
+# print(X_train_post.shape)
+# print(X_test_post.shape)
+# X_train_post['SalePrice'] = pd.Series(train_data_raw['SalePrice'])
+X_train_post['SalePrice'] = train_data_raw['SalePrice']
+# X_train_post.to_csv('Check.csv', index=False)
+
+# print(X_train_post.shape)
+# print(X_test_post.shape)
+
+X_train_post = X_train_post[features]
+
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X_train_post, y, random_state=1)
+
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state=1, n_estimators=70)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+
+
+# To improve accuracy, create a new Random Forest model which you will train on all training data
+rf_model_on_full_data = RandomForestRegressor(random_state=1, n_estimators=70)
+
+# fit rf_model_on_full_data on all data from the training data
+rf_model_on_full_data.fit(X_train_post,y)
+
+# create test_X which comes from test_data but includes only the columns you used for prediction.
+# The list of columns is stored in a variable called features
+test_X = X_test_post[features]
+
+# make predictions which we will submit. 
+test_preds = rf_model_on_full_data.predict(test_X)
+
+# The lines below shows how to save predictions in format used for competition scoring
+# Just uncomment them.
+
+output = pd.DataFrame({'Id': test_data.Id,
+                      'SalePrice': test_preds})
+output.to_csv('submission.csv', index=False)
+print("DONE")
 
